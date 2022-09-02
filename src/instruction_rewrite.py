7db7570dc149bce6526909d217c5a435af62faf1
Lucas Emmes <lucas.f.emmes@protonmail.com>
2022-09-02 11:36:58 +0200

    Started rewriting instructions for the 4th time...

---
+++ src/instruction_rewrite.py
@@ -0,0 +1,196 @@
+
+addresses = {"#A2":0,"#B2":1,"#M":2,"#A":3,"#B":4,"#C":5,"#D":6,"#D2":7}
+next_var_address = len(addresses)
+
+# ------------------------------------------------------------------------------------------------------------
+# META FUNCTIONS ---------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+
+def META_ADD_VARIABLE(VAR_NAME):
+    """Parses the name of VAR_NAME and its size, then registers it in the index of known variables"""
+    global addresses
+    global next_var_address
+    if "[" in VAR_NAME:
+        VAR_NAME, VAR_SIZE = VAR_NAME[:-1].split("[")
+        if VAR_NAME in addresses: raise Exception(f"Redeclaration of {VAR_NAME}")
+        addresses[VAR_NAME] = next_var_address
+        next_var_address += int(VAR_SIZE)
+    else:
+        if VAR_NAME in addresses: raise Exception(f"Redeclaration of {VAR_NAME}")
+        addresses[VAR_NAME] = next_var_address
+        next_var_address += 1
+
+
+def META_GET_VARIABLE_ADDRESS(VAR_NAME):
+    """Retrieves the numerical address / index of the variable X"""
+    if "[" in VAR_NAME:
+        VAR_NAME, VAR_SIZE = VAR_NAME[:-1].split("[")
+        if VAR_NAME not in addresses: raise Exception(f"{VAR_NAME} has not been declared")
+        return addresses[VAR_NAME] + int(VAR_SIZE)
+    else:
+        if VAR_NAME not in addresses: raise Exception(f"{VAR_NAME} has not been declared")
+        return addresses[VAR_NAME]
+
+
+# ------------------------------------------------------------------------------------------------------------
+# CODE FUNCTIONS ---------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+
+def COMMAND_MOV(X, Y):
+    """Move contents of cell X into Y, clearing X and overwriting Y  
+    Same as Y=X, X=0"""
+    return  MICRO_SETVALUE(Y, 0)    +\
+            HELPER_DISTRIBUTE_INTO(X, Y)
+
+def COMMAND_ADD(X, Y, Z):
+    """Add X to Y, put result into Z  
+    Same as Z = (X + Y)"""
+    return
+
+def COMMAND_SUB(X, Y, Z):
+    """Subtract Y from X, put result into Z"""
+    pass
+
+def COMMAND_MUL(X, Y, Z):
+    """Multiply X with Y and put result into Z"""
+    pass
+
+def COMMAND_DIV(X, Y, Z):
+    """(Whole number) Divide X by Y and put result into Z"""
+    pass
+
+def COMMAND_MOD(X, Y, Z):
+    """Finds X modulo Y and puts it into Z"""
+    pass
+
+def COMMAND_INP(X):
+    """Take input from the console and store in X"""
+    return  MICRO_INPUT(X)
+
+def COMMAND_OUT(X):
+    """Print X to the console"""
+    return  MICRO_OUTPUT(X)
+
+def COMMAND_SET(X, V):
+    """Set X to value V"""
+    return  MICRO_SETVALUE(X, V)
+
+def COMMAND_CPY(X, Y):
+    """Copy contents of cell X into Y, leaving X seemingly untouched and overwriting Y"""
+    return  HELPER_DISTRIBUTE_INTO(X, 2, Y) +\
+            COMMAND_MOV(2, X)
+
+
+# ------------------------------------------------------------------------------------------------------------
+# HELPING FUNCTIONS ------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+
+def HELPER_DISTRIBUTE_INTO(X, *args):
+    """Will take X and distribute it into multiple other cells"""
+    return  FORMULATE_FOR_LOOP(X, 1,
+        "".join([
+            MICRO_INCREMENT(pos, 1) for pos in args
+            ]))
+
+
+# ------------------------------------------------------------------------------------------------------------
+# FORMULATING FUNCTIONS --------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+
+def FORMULATE_IF_ELSE(CONDITION, INSTRUCTIONS_IF, INSTRUCTIONS_ELSE):
+    """Formulates an if/else statement"""
+    pass
+
+def FORMULATE_FOR_LOOP(X, DECREMENT_AMOUNT, INSTRUCTIONS_LOOP):
+    """Formulates a for-loop running over X and decrementing it DECREMENT_AMOUNT times each iteration.  
+    WILL AUTOMATICALLY MOVE TO X BEFORE STARTING THE LOOP AND AT THE END OF LOOP"""
+    return  MICRO_PTR_GOTO(X)       +\
+            "["                     +\
+            MICRO_PTR_GOTO(-X)      +\
+            INSTRUCTIONS_LOOP       +\
+            MICRO_PTR_GOTO(X)       +\
+            "-"*DECREMENT_AMOUNT    +\
+            "]"                     +\
+            MICRO_PTR_GOTO(-X)
+
+
+# ------------------------------------------------------------------------------------------------------------
+# ALU FUNCTIONS ----------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+
+def ALU_ADD():
+    """Add A and B into C, consuming A and B"""
+    return  HELPER_DISTRIBUTE_INTO(3, 5)    +\
+            HELPER_DISTRIBUTE_INTO(4, 5)
+
+def ALU_SUB():
+    """Subtract B from A and put into C"""
+    return  HELPER_DISTRIBUTE_INTO(3, 5)    +\
+            FORMULATE_FOR_LOOP(4, 1, MICRO_DECREMENT(5, 1))
+
+def ALU_MUL():
+    """Multiply X and Y and put into C"""
+    pass
+
+def ALU_DIV():
+    """Divide X by Y and put into C"""
+    pass
+
+def ALU_MODULO():
+    """Calculates A modulo B and puts it into C"""
+    pass
+
+def ALU_CMP_A_GREATER_THAN_B():
+    """Checks if A is larger than B, putting result into D"""
+    pass
+
+def ALU_CMP_A_EQUALS_B():
+    """Checks if A is equal to B, putting result into D"""
+    return 
+
+
+# ------------------------------------------------------------------------------------------------------------
+# MICRO FUNCTIONS --------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------
+
+
+def MICRO_PTR_GOTO(POSITION):
+    """Move pointer to position X"""
+    if POSITION < 0:    return "<" * -POSITION
+    else:               return ">" * POSITION
+
+def MICRO_INCREMENT(X, N):
+    """Increments the current cell by N"""
+    return  MICRO_PTR_GOTO(X)   +\
+            "+" * N             +\
+            MICRO_PTR_GOTO(-X)
+
+def MICRO_DECREMENT(X, N):
+    """Decrements the current cell by N"""
+    return  MICRO_PTR_GOTO(X)   +\
+            "-" * N             +\
+            MICRO_PTR_GOTO(-X)
+
+def MICRO_SETVALUE(X, N):
+    return  MICRO_PTR_GOTO(X)                       +\
+            ("[-]" if N == 0 else "")               +\
+            ("[-]" + "+" * N if N < 128 else "")    +\
+            ("[-]" + "-" * N if N >= 128 else "")   +\
+            MICRO_PTR_GOTO(-X)
+
+def MICRO_INPUT(X):
+    """Takes input from terminal and stores it in the current cell"""
+    return  MICRO_PTR_GOTO(X)   +\
+            ","                 +\
+            MICRO_PTR_GOTO(-X)
+
+def MICRO_OUTPUT(X):
+    """Takes the current cell and outputs it to terminal"""
+    return  MICRO_PTR_GOTO(X)   +\
+            "."                 +\
+            MICRO_PTR_GOTO(-X)
--- src/testing.py
+++ src/testing.py
@@ -1,4 +1,4 @@
-from instructions import *
+from instruction_rewrite import *
 
 def optimize(program):
     new = program
@@ -7,15 +7,97 @@ def optimize(program):
         new = new.replace("-+", "")
         new = new.replace("<>", "")
         new = new.replace("><", "")
+    
+    while (new[-1] == "<" or new[-1] == ">"):
+        new = new[:-1]
+
     return new
+
+def e(a, b, d):
+    (i:=0,s:=[],c:=0,p:=0,f:=[0])
+    while i<len(d):
+        c=d[i]
+        if c==">":p+=1;f.append(0)if p==len(f)else 0
+        if c=="<":p-=1 if p>0 else f.insert(0,0)
+        if c=="+":f[p]=f[p]+1%256
+        if c=="-":f[p]=f[p]-1%256
+        if c==".":print(chr(f[p]),end="")
+        if c==",":e=input();f[p]=(ord(e[0])if len(e)>0 else 0)%256
+        if c=="[":
+            if f[p]:s.append(i-1)
+            else:
+                c=1
+                while c!=0:
+                    i+=1
+                    if d[i]=="[":c+=1
+                    elif d[i]=="]":c-=1
+        if c=="]":i=s.pop()
+        i+=1
+    return (f[a:b])
+
+def assert_eq(i, a, b):
+    if a != b:
+        raise Exception(f"Failed test {i}\nShould be {a}\nBut was   {b}")
+    else:
+        print(f"Passed test {i}")
+
+def TESTS():
+    t1 = "".join([
+        MICRO_SETVALUE(5,5),
+        MICRO_SETVALUE(3,5)
+        ])
+    r1 = e(0, 6, optimize(t1))
+    s1 = [0,0,0,5,0,5]
+    assert_eq(1, s1, r1)
+
+    t2 = "".join([
+        MICRO_SETVALUE(2,10),
+        MICRO_DECREMENT(2, 5)
+        ])
+    r2 = e(0,3, optimize(t2))
+    s2 = [0,0,5]
+    assert_eq(2, s2, r2)
+
+    t3 = "".join([
+        MICRO_SETVALUE(1,5),
+        FORMULATE_FOR_LOOP(1,1, MICRO_INCREMENT(4, 5))
+        ])
+    r3 = e(0, 5, optimize(t3))
+    s3 = [0, 0, 0, 0, 25]
+    assert_eq(3, s3, r3)
+
+    t4 = "".join([
+        MICRO_SETVALUE(2, 10),
+        HELPER_DISTRIBUTE_INTO(2, 0, 4)
+        ])
+    r4 = e(0, 5, optimize(t4))
+    s4 = [10, 0, 0, 0, 10]
+    assert_eq(4, s4, r4)
+
+    t5 = MICRO_SETVALUE(0, 10) + COMMAND_MOV(0, 1)
+    r5 = e(0,2,optimize(t5))
+    s5 = [0, 10]
+    assert_eq(5, s5, r5)
+
+    t6 = MICRO_SETVALUE(5, 10) + COMMAND_CPY(5, 7)
+    r6 = e(0, 8, optimize(t6))
+    s6 = [0,0,0,0,0,10,0,10]
+    assert_eq(6, s6, r6)
+
+    t7 = COMMAND_SET(3, 4) + COMMAND_SET(4, 5) + ALU_ADD()
+    r7 = e(0,6,optimize(t7))
+    s7 = [0,0,0,0,0,9]
+    assert_eq(7, s7, r7)
+
+    t8 = COMMAND_SET(3, 9) + COMMAND_SET(4, 5) + ALU_SUB()
+    r8 = e(0,6,optimize(t8))
+    s8 = [0,0,0,0,0,4]
+    assert_eq(8, s8, r8)
 
 
 def main():
 
-    print(optimize(
-        COMMAND_SET(0, 10) +
-        HELPER_DISTRIBUTE_INTO(0, 5, 6, 1, 2, 3, 4)
-    ))
+    TESTS()
 
 if __name__ == '__main__':
     main()
